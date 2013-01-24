11aa9134403f25c2dc27f485bf28b631394c8079
Kyle & Sarah <kyle@lucovsky.com>
2013-01-24 10:56:11 -0800

    initial commit, set up db migrations for url shortener

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+URLShortener::Application.config.secret_token = 'ad856eb4e81e18e25a9b5c789a43331a5140e4190571c7a020716080b74f83a7746eeb516bdac30032512e405c6a5fa0f1107c9cb6052cccea32c444ba1fb086'
---
+++ db/migrate/20130124184841_create_ur_ls.rb
@@ -0,0 +1,7 @@
+class CreateUrLs < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
